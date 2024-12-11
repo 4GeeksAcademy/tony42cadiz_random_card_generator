4ec42c83aa2fd4aa714e8ce7189d10a8794bdc77
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2024-12-11 18:45:02 +0000

    Random_Card_Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function random(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+let numeros = [
+  "A",
+  "J",
+  "Q",
+  "K",
+  "10",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9"
+];
+
+let palos = ["♦", "♥", "♠", "♣"];
+
+function actualizarCarta() {
+  let paloRandom = random(palos);
+  let numeroRandom = random(numeros);
+
+  document.getElementById("top-suit").textContent = paloRandom;
+  document.getElementById("card-number").textContent = numeroRandom;
+  document.getElementById("butom-suit").textContent = paloRandom;
+
+  if (paloRandom === "♦" || paloRandom === "♥") {
+    document.getElementById("top-suit").style.color = "red";
+    document.getElementById("butom-suit").style.color = "red";
+  } else {
+    document.getElementById("top-suit").style.color = "black";
+    document.getElementById("butom-suit").style.color = "black";
+  }
+}
+
+actualizarCarta();
